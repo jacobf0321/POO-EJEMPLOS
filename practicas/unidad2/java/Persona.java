cc395b1bf037f621292cf329ec37bc5a4407795d
jacobFlores <jacobr.floresm@gmail.com>
2023-10-26 20:23:28 -0700

    Se agrega clase persona en Java

--- practicas/unidad2/java/Persona.java
+++ practicas/unidad2/java/Persona.java
@@ -1,39 +1,44 @@
-class Persona {
+class Persona{
+public static void main(String[] args) {
+    Persona persona = new Persona();
+
+    persona.edad = 25;
+    persona.nombre = "Rogelio";
+    persona.vive = true;
+
+    System.out.println("La edad de la persona es " + persona.edad);
+    System.out.println("¿La persona vive? " + persona.vive);
+
+    persona.saludar();
+
+    System.out.println("La edad de la persona es " + persona.edad);
+    System.out.println("¿La persona vive? " + persona.vive);
+
+    persona.incrementarEdad();
+
+    System.out.println("La edad de la persona es " + persona.edad);
+    System.out.println("¿La persona vive? " + persona.vive);
+    
+    persona.morir();
+
+    System.out.println("La edad de la persona es " + persona.edad);
+    System.out.println("¿La persona vive? " + persona.vive);
+}
+
     String nombre;
     int edad;
     boolean vive;
 
-    void saludar(){
-        System.out.print("Hola, mi nombre es " + nombre);
+    void saludar() {
+        System.out.println("Hola, mi nombre es "+ nombre);
     }
-    void incrementarEdad(){
+
+    void incrementarEdad() {
         edad = edad + 1;
     }
-    void morir(){
+
+    void morir() {
         vive = false;
     }
+
 }
-
-    public static void main(String[] args){
-        Persona persona = new Persona();
-
-        persona.edad = 25;
-        persona.nombre = "Roge";
-        persona.vive = true;
-
-        System.out.print("La edad es " + persona.edad);
-        System.out.print("Esta vivo " + persona.vive);
-        persona.saludar();
-
-        System.out.print("La edad es " + persona.edad);
-        System.out.print("Esta vivo " + persona.vive);
-        persona.incrementarEdad();
-
-        System.out.print("La edad es " + persona.edad);
-        System.out.print("Esta vivo " + persona.vive);
-        persona.morir();
-
-        System.out.print("La edad es " + persona.edad);
-        System.out.print("Esta vivo " + persona.vive);
-
-    }
