74a6e85ab8955e8e8621fbc52a3b3356b74e665c
jacobFlores <jacobr.floresm@gmail.com>
2023-10-26 20:50:16 -0700

    Se da formato a Persona.Java

--- practicas/unidad2/java/Persona.java
+++ practicas/unidad2/java/Persona.java
@@ -1,36 +1,36 @@
-class Persona{
-public static void main(String[] args) {
-    Persona persona = new Persona();
+class Persona {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
 
-    persona.edad = 25;
-    persona.nombre = "Rogelio";
-    persona.vive = true;
+        persona.edad = 25;
+        persona.nombre = "Rogelio";
+        persona.vive = true;
 
-    System.out.println("La edad de la persona es " + persona.edad);
-    System.out.println("¿La persona vive? " + persona.vive);
+        System.out.println("La edad de la persona es " + persona.edad);
+        System.out.println("¿La persona vive? " + persona.vive);
 
-    persona.saludar();
+        persona.saludar();
 
-    System.out.println("La edad de la persona es " + persona.edad);
-    System.out.println("¿La persona vive? " + persona.vive);
+        System.out.println("La edad de la persona es " + persona.edad);
+        System.out.println("¿La persona vive? " + persona.vive);
 
-    persona.incrementarEdad();
+        persona.incrementarEdad();
 
-    System.out.println("La edad de la persona es " + persona.edad);
-    System.out.println("¿La persona vive? " + persona.vive);
-    
-    persona.morir();
+        System.out.println("La edad de la persona es " + persona.edad);
+        System.out.println("¿La persona vive? " + persona.vive);
 
-    System.out.println("La edad de la persona es " + persona.edad);
-    System.out.println("¿La persona vive? " + persona.vive);
-}
+        persona.morir();
+
+        System.out.println("La edad de la persona es " + persona.edad);
+        System.out.println("¿La persona vive? " + persona.vive);
+    }
 
     String nombre;
     int edad;
     boolean vive;
 
     void saludar() {
-        System.out.println("Hola, mi nombre es "+ nombre);
+        System.out.println("Hola, mi nombre es " + nombre);
     }
 
     void incrementarEdad() {
